ecda0ca9b2dae87aeab09156215f8b090f1043c6
Hiroki Teranishi <teranishi.hiroki@gmail.com>
2023-01-13 10:11:20 +0900

    Refactor.

--- pylsp_isort/plugin.py
+++ pylsp_isort/plugin.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from pathlib import Path
-from typing import Any, Dict, Generator, TypedDict
+from typing import Any, Dict, Generator, Optional, TypedDict, Union
 
 import isort
 from pylsp import hookimpl
@@ -39,38 +39,72 @@ def pylsp_format_document(config: Config, document: Document) -> Generator:
         end={"line": len(document.lines), "character": 0},
     )
     outcome = yield
-    _process(outcome, config, document, range)
+    _format(outcome, config, document, range)
 
 
 @hookimpl(hookwrapper=True)
 def pylsp_format_range(config: Config, document: Document, range: Range) -> Generator:
     outcome = yield
-    _process(outcome, config, document, range)
+    _format(outcome, config, document, range)
 
 
-def _process(outcome, config: Config, document: Document, range: Range):
+def _format(outcome, config: Config, document: Document, range: Range) -> None:
     result = outcome.get_result()
     if result:
         text = result[0]["newText"]
         range = result[0]["range"]
     else:
-        start = range["start"]["line"]
-        end = range["end"]["line"]
-        text = "".join(document.lines[start:end])
+        text = "".join(document.lines[range["start"]["line"] : range["end"]["line"]])
 
-    config_kwargs = {}
-    defined_args = set(getattr(isort.Config, "__dataclass_fields__", {}).keys())
     settings = config.plugin_settings("isort", document_path=document.path)
-    for key, value in settings.items():
-        if key in defined_args:
-            config_kwargs[key] = value
-    config_kwargs["settings_path"] = os.path.dirname(os.path.abspath(document.path))
-    logger.debug("config_kwargs=%r", config_kwargs)
-
-    new_text = isort.code(
-        text, config=isort.Config(**config_kwargs), file_path=Path(document.path)
-    )
+    new_text = run_isort(text, settings, file_path=document.path)
 
     if new_text != text:
         result = [{"range": range, "newText": new_text}]
         outcome.force_result(result)
+
+
+def run_isort(
+    text: str,
+    settings: Optional[Dict[str, Any]] = None,
+    file_path: Optional[Union[str, bytes, os.PathLike]] = None,
+) -> str:
+    config = isort_config(settings or {}, file_path)
+    file_path = Path(os.fsdecode(file_path)) if file_path else None
+    return isort.code(text, config=config, file_path=file_path)
+
+
+def isort_config(
+    settings: Dict[str, Any],
+    target_path: Optional[Union[str, bytes, os.PathLike]] = None,
+) -> isort.Config:
+    config_kwargs = {}
+    unsupported_kwargs = {}
+
+    defined_args = set(getattr(isort.Config, "__dataclass_fields__", {}).keys())
+    for key, value in settings.items():
+        if key in defined_args:
+            config_kwargs[key] = value
+        else:
+            unsupported_kwargs[key] = value
+
+    if "settings_path" in settings:
+        if os.path.isfile(settings["settings_path"]):
+            config_kwargs["settings_file"] = os.path.abspath(settings["settings_path"])
+            config_kwargs["settings_path"] = os.path.dirname(
+                config_kwargs["settings_file"]
+            )
+        else:
+            config_kwargs["settings_path"] = os.path.abspath(settings["settings_path"])
+    elif target_path:
+        config_kwargs["settings_path"] = os.path.abspath(target_path)
+        if not os.path.isdir(config_kwargs["settings_path"]):
+            config_kwargs["settings_path"] = os.path.dirname(
+                config_kwargs["settings_path"]
+            )
+
+    logger.debug("config_kwargs=%r", config_kwargs)
+    if unsupported_kwargs:
+        logger.info("unsupported_kwargs=%r", unsupported_kwargs)
+
+    return isort.Config(**config_kwargs)
